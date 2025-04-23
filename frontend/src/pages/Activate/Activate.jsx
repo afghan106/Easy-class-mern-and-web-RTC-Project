17abc5425d6d009d2198102b5d724404f5520dc7
afghan808106 <afghansa106@gmail.com>
2025-04-23 02:19:22 -0700

    create the activate file to navigate into its steps

--- frontend/src/pages/Activate/Activate.jsx
+++ frontend/src/pages/Activate/Activate.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Name from "../steps/name/Name.jsx";
+import Avatar from "../steps/avatar/Avatar.jsx";
 
 const Activate = () => {
-  return (
-    <div> this is Activate</div>
-  )
-}
+    const [step, setStep] = useState(1);
 
-export default Activate
+    const steps = {
+        1: Name,
+        2: Avatar,
+    };
+
+    const Step = steps[step];
+
+    const nextStep = () => {
+        if (step < Object.keys(steps).length) {
+            setStep(step + 1);
+            
+        }
+    };
+    const back = () => {
+        if (step < Object.keys(steps).length) {
+            setStep(step - 1);
+
+        }
+    };
+
+    return (
+       
+            <Step back={back} nextStep={nextStep} />
+       
+    );
+};
+
+export default Activate;

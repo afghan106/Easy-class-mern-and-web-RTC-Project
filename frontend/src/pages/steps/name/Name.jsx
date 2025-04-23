1cebde23b9dfaf454005bbb528f50fa23f217aa7
afghan808106 <afghansa106@gmail.com>
2025-04-23 02:26:40 -0700

    name component created successfully

--- frontend/src/pages/steps/name/Name.jsx
+++ frontend/src/pages/steps/name/Name.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from '../../../components/shared/Card/Card'
+import TextInput from '../../../components/shared/TextInput/TextInput'
+import Button from '../../../components/shared/button/Button'
 
-const Name = () => {
+const Name = ({nextStep}) => {
   return (
-    <div>Name</div>
+    <Card title={"Enter Your Real FullName"}>
+      <TextInput placeholder={"FullName"}></TextInput>
+      <p style={{color:"#6799ff"}}>this name is too important for certifications!</p>
+      <Button text={'Next'} onclick={nextStep}></Button>
+    </Card>
   )
 }
 
